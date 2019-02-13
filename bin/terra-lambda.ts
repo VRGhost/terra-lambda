6ed4301ba9283038cc9e3aa1e19ba9ba874ef069
Ilja Orlovs <vrghost@gmail.com>
2019-02-13 16:18:12 +0000

    added branch name postfix

--- bin/terra-lambda.ts
+++ bin/terra-lambda.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import cdk = require('@aws-cdk/cdk');
+import branch = require('git-branch');
+
 import { TheStack } from '../lib/terra-lambda-stack';
 
 const app = new cdk.App();
-new TheStack(app, 'TerraLambdaStack');
+new TheStack(app, 'TerraLambdaStack-' + branch.sync());
 app.run();
