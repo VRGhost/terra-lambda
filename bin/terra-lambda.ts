df4ab7215fbbd3b311e1eadb330b29c8a6d28c1b
Ilja Orlovs <vrghost@gmail.com>
2019-02-13 15:34:13 +0000

    added ts

--- bin/terra-lambda.ts
+++ bin/terra-lambda.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import cdk = require('@aws-cdk/cdk');
-import { TmpStack } from '../lib/_tmp-stack';
+import { TheStack } from '../lib/terra-lambda-stack';
 
 const app = new cdk.App();
-new TmpStack(app, 'TerraLambdaStack');
+new TheStack(app, 'TerraLambdaStack');
 app.run();
