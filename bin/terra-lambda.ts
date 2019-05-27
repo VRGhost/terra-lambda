caabd24fea3125c818edcebdcd37e3d1d43fcd56
Ilja Orlovs <vrghost@gmail.com>
2019-05-27 12:19:32 +0100

    Version bump

--- bin/terra-lambda.ts
+++ bin/terra-lambda.ts
@@ -1,10 +1,23 @@
-#!/usr/bin/env node
 import 'source-map-support/register';
+import yargs = require('yargs');
+
 import cdk = require('@aws-cdk/cdk');
+import s3 = require('@aws-cdk/aws-s3');
+
 import branch = require('git-branch');
 
 import { TheStack } from '../lib/terra-lambda-stack';
 
-const app = new cdk.App();
-new TheStack(app, 'TerraLambdaStack-' + branch.sync());
-app.run();
+class MyApp extends cdk.App {
+    constructor(_: string[]) {
+        super();
+        const stack = new TheStack(this, 'TerraLambdaStack-' + branch.sync());
+
+        new s3.Bucket(stack, 'force-update-2', {
+            removalPolicy: cdk.RemovalPolicy.Destroy
+        }); // XXX: Amend this bucket's name to force stack update
+    }
+}
+
+const app = new MyApp(process.argv);
+app.run()
