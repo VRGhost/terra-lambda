e9acb19eadc0a07fe5598866e07689a279099d5a
Ilja Orlovs <vrghost@gmail.com>
2019-07-07 18:09:44 +0100

    Restored back into working

--- bin/terra-lambda.ts
+++ bin/terra-lambda.ts
@@ -1,23 +1,23 @@
 import 'source-map-support/register';
-import yargs = require('yargs');
+//import yargs = require('yargs');
 
-import cdk = require('@aws-cdk/cdk');
+import core = require('@aws-cdk/core');
 import s3 = require('@aws-cdk/aws-s3');
 
 import branch = require('git-branch');
 
 import { TheStack } from '../lib/terra-lambda-stack';
 
-class MyApp extends cdk.App {
+class MyApp extends core.App {
     constructor(_: string[]) {
         super();
         const stack = new TheStack(this, 'TerraLambdaStack-' + branch.sync());
 
         new s3.Bucket(stack, 'force-update-2', {
-            removalPolicy: cdk.RemovalPolicy.Destroy
+            removalPolicy: core.RemovalPolicy.DESTROY
         }); // XXX: Amend this bucket's name to force stack update
     }
 }
 
 const app = new MyApp(process.argv);
-app.run()
+app.synth()
